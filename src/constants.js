d535e169b9e185efc79144a4e196791bf7472803
AbhinandNtk <abhinadntkm@gamail.com>
2023-11-10 15:12:43 +0530

    first commit

---
+++ src/constants.js
@@ -0,0 +1,9 @@
+const baseUrl = "https://api.themoviedb.org/3";
+const API_KEY = `${process.env.REACT_APP_API_KEY}`;
+// const API_KEY = "426308d2cbf3338c94cbcc6304da2631";
+const imageUrl = "https://image.tmdb.org/t/p/original";
+const w300ImageUrl = "https://image.tmdb.org/t/p/w300";
+
+export { baseUrl, API_KEY, imageUrl, w300ImageUrl };
+
+
